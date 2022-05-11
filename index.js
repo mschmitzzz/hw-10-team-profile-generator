abb5b941f18b065c7a4feface71975d406503981
Mac Schmitz <mac.schmitz@att.net>
2022-05-10 22:24:06 -0400

    Finished

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function createManager() {
         {
             type: 'input',
             name: 'managerName',
+            message: "What is the team manager's name?",
             validate: (answer) => {
                 if (answer !== '') {
                     return true;
@@ -69,7 +70,163 @@ function createManager() {
         )
         teamMembers.manager = manager;
         idArray.push(answers.managerId);
-        buildTeam();
+        createTeam();
+    });
+}
+
+function createTeam() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'Which type of team member would you like to add?',
+            choices: [
+                'Engineer',
+                'Intern',
+                'I do not want to add any more team members'
+            ]
+        }
+    ]).then((answers) => {
+        switch (answers.choice) {
+            case 'Engineer':
+                addEngineer();
+                break;
+            case 'Intern':
+                addIntern();
+                break;
+            default:
+                buildTeam();
+        }
+    })
+}
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?",
+            validate: (answer) => {
+                if (answer !== '') {
+                    return true;
+                }
+                return "Please enter at least one character";
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the engineer's id?",
+            validate: (answer) => {
+                const pass = answer.match(/^[1-9]\d*$/)
+                if (pass) {
+                    if (idArray.includes(answer)) {
+                        return "This id is already taken"
+                    } else {
+                        return true;
+                    }
+                }
+                return "Please enter a positive number greater than 0"
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's email address?",
+            validate: (answer) => {
+                const pass = answer.match(/\S+@\S+\.\S+/)
+                if (pass) {
+                    return true
+                }
+                return "Please enter a valid email address"
+            },
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's Github username?",
+            validate: (answer) => {
+                if (answer !== '') {
+                    return true;
+                }
+                return "Please enter at least one character";
+            },
+        },
+    ]).then((answers) => {
+        const engineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.github
+        )
+        teamMembers.engineers.push(engineer);
+        idArray.push(answers.id);
+        createTeam();
+    });
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?",
+            validate: (answer) => {
+                if (answer !== '') {
+                    return true;
+                }
+                return "Please enter at least one character";
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the intern's id?",
+            validate: (answer) => {
+                const pass = answer.match(/^[1-9]\d*$/)
+                if (pass) {
+                    if (idArray.includes(answer)) {
+                        return "This id is already taken"
+                    } else {
+                        return true;
+                    }
+                }
+                return "Please enter a positive number greater than 0"
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the intern's email address?",
+            validate: (answer) => {
+                const pass = answer.match(/\S+@\S+\.\S+/)
+                if (pass) {
+                    return true
+                }
+                return "Please enter a valid email address"
+            },
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Where is the intern enrolled in school?",
+            validate: (answer) => {
+                if (answer !== '') {
+                    return true;
+                }
+                return "Please enter at least one character";
+            },
+        },
+    ]).then((answers) => {
+        const intern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.school
+        )
+        teamMembers.interns.push(intern);
+        idArray.push(answers.id);
+        createTeam();
     });
 }
 
--- src/page-template.js
+++ src/page-template.js
@@ -16,10 +16,42 @@ function renderManager(manager) {
     `;
 }
 function renderEngineers(engineers){
-    return `engineer`;
+    const markup = engineers.map((engineer) => {
+        return `<div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div>
+        `
+    });
+    return markup.join(',');
 }
 function renderInterns(interns) {
-    return `intern`;
+    const markup = interns.map((intern) => {
+        return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>`
+});
+return markup.join(',');
 }
 
 function generateTeam(teamMembers) {
